3c9f1fad6635310ff51de37b24bd1c6a7f1d85f5
Richard Siegel <richard@siegel.work>
2020-12-04 17:14:38 +0100

    fixed note selection bug

--- src/main.cpp
+++ src/main.cpp
@@ -25,12 +25,15 @@ int main(int argc, char *argv[])
     NcursesTerminalIO cursesTerm;
     int notesToSkip = 0;     // number of itemes, which is 'scrolled' out of view at the top.
     char selectedIndex = -1; // the number used to select an item (-1 means no selection)
+    int noteIndex = -1;      // index of the selected note
     char keyInput = -1;      // the last key entered
     bool runningMainLoop = true;
     TodoNote selectedNote;
     while (runningMainLoop)
     {
         std::vector<TodoNote> notes = storage.getNotes();
+        noteIndex = selectedIndex + notesToSkip - 1;
+        selectedNote = (noteIndex > -1 && noteIndex < notes.size())? notes[noteIndex] : TodoNote();
         cursesTerm.displayNotes(notes, notesToSkip, selectedIndex);
         keyInput = cursesTerm.awaitKeyPress();
         switch (keyInput)
@@ -55,14 +58,7 @@ int main(int argc, char *argv[])
             break;
         default:
             if (keyInput >= '1' && keyInput <= '9') // select item if key was a number
-            {
                 selectedIndex = keyInput - '0';
-                selectedNote = notes[selectedIndex + notesToSkip - 1];
-            }
-            else
-            {
-                selectedNote = TodoNote();
-            }
             // else TODO implement default case
             break;
         }
